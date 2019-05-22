b589cff623943c67c274b512f9afb38dcea0e9d8
masahiro9705 <masahiro970513@gmail.com>
2019-05-22 11:16:05 +0900

    最初のコミット

---
+++ app/src/main/java/com/example/sainttropez/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.sainttropez
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.Menu
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl("file:///android_asset/html/index.html")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+}
